10a9b8ee78f7391761a71c9a2060e372b12b3a53
elenaorsini <59398721+ele-max@users.noreply.github.com>
2024-07-18 12:19:58 +0200

    sseq.py

--- sseq.py
+++ sseq.py
@@ -1,19 +1,16 @@
 def read_fasta(s):
-    
     # initialize the dictionary
     d = {}
     # initialize the sequence name
     name = ''
-    # loop over the lines in the file
+   
     s = s.split('\n')
     for line in s:
         if line == '':
             continue
-        # remove the newline character
+    
         line = line.rstrip()
-        # if the line starts with '>'
-        if line.startswith('>'):
-            # set the sequence name
+       if line.startswith('>'):
             name = line[1:]
             # initialize the sequence
             d[name] = ''
@@ -22,6 +19,7 @@ def read_fasta(s):
             # append the sequence
             d[name] += line
     return d
+
 with open('rosalind_sseq.txt') as file:
     txt = file.read()
     d = read_fasta(txt)
@@ -29,7 +27,16 @@ with open('rosalind_sseq.txt') as file:
 s, t = list(d.values())
 
 indices = []
+j = 0
 for i in range(len(t)):
-    v = s.index(t[i], indices[-1]+1 if len(indices) > 0 else 0)
-    indices.append(v)
-print(' '.join(list(map(lambda k: str(k), indices))))
+    found = False
+    for k in range(j, len(s)):
+        if t[i] == s[k]:
+            indices.append(k + 1)
+            j = k + 1
+            found = True
+            break
+    if not found:
+        break
+
+print(' '.join(list(map(str, indices))))
